93b1a90a4a6db1d0d12f828d0eb06df1b8cf4b7a
mazzu <denismazzu@gmail.com>
2018-12-12 15:03:18 +0100

    add test for def list

--- test/DefSpec.hs
+++ test/DefSpec.hs
@@ -10,6 +10,7 @@ import Parser
 import DerivedPrimitives
 import Control.Applicative
 import Token
+import ParseUtility
 import ParseProg
 
 --
@@ -20,4 +21,4 @@ tests = [
   ]
 
 c0 = assertT "[Def] one instance" [(("name", ENum 3), "abc")] (parse parseDef "name = 3 abc")
-c1 = assertT "[Def] list" [([("name", ENum 3), ("x2_S", ENum 2)], "; abc")] (parse parseDefs "name = 3  ; x2_S = 2; abc")
+c1 = assertT "[Def] list" [([("name", ENum 3), ("x2_S", ENum 2)], "; abc")] (parse (parseSemiColonList parseDef) "name = 3  ; x2_S = 2; abc")
