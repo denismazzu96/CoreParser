f310a73359c581d6481f7ecc601b75d5cafcdd10
mazzu <denismazzu@gmail.com>
2018-12-13 17:47:54 +0100

    add small example test

--- test/ProgramSpec.hs
+++ test/ProgramSpec.hs
@@ -15,10 +15,12 @@ import ParseProg
 --
 
 tests = [
+  c1,
   c0
   ]
 
-c0 = assertT "[Program] INTEGRATION TEST" out (parse parseProg p)
+c0 = assertT "[Program] FILE' INTEGRATION TEST" out (parse parseProg p)
+c1 = assertT "[Program] small example" out1 (parse parseProg p1)
 
 p = "f = 3;\ng x y = let z = x in z;\nh x = case (let y = x in y) of\n<1> -> 2\n<2> -> 5"
 out = [(three, "")]
@@ -49,3 +51,8 @@ three = [
   )
   ]
 
+p1 = "main = double 21 ;\ndouble x = x+x"
+out1 = [([
+    ("main", [], (EAp (EVar "double") (ENum 21))),
+    ("double", ["x"], (EAp (EAp (EVar "+") (EVar "x")) (EVar "x")))
+  ], "")]
