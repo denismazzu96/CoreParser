a7211f95bf3aa1693ebeba5034fe34434eaad2b5
mazzu <denismazzu@gmail.com>
2018-12-13 15:56:04 +0100

    add core-language keyword

--- src/Language.hs
+++ src/Language.hs
@@ -2,9 +2,10 @@ module Language where
 
 --
 
+type Triple a b = (a, [b], Expr b)
 type Name = String -- for string name
 type Def a = (a, Expr a) -- for let and letrec
-type Alter a = (Int, [a], Expr a) -- for case
+type Alter a = Triple Int a -- for case
 data IsRec = NonRecursive | Recursive deriving (Show, Eq) -- for let and letrec
 type CoreExpr = Expr Name
 
@@ -25,6 +26,9 @@ type CoreProgram = Program Name
 
 --
 
-type ScDefn a = (Name, [a], Expr a) -- definition similar to alter TODO: combine definition with alter in more general way
+type ScDefn a = Triple Name a
 type CoreScDefn = ScDefn Name
 
+--
+keywords :: [String]
+keywords = ["let", "letrec", "case", "in", "of", "Pack"]
